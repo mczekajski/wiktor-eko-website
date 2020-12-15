8558766ea4e420d7ca7f6a5156ca526d7e285e93
Marcin Czekajski <czekajskimarcin@gmail.com>
2020-12-15 19:41:32 +0100

    Add mobile menu

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const menuButton = document.querySelector(".header__menu-btn");
+const menu = document.querySelector(".header__nav-menu")
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle("hidden");
+})
