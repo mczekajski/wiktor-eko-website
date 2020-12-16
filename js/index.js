b704c088433e122e9b2a09c640bd12b16d887eb2
Marcin Czekajski <czekajskimarcin@gmail.com>
2020-12-16 09:37:02 +0100

    Finish mobile view

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const menuButton = document.querySelector(".header__menu-btn");
 const menu = document.querySelector(".header__nav-menu")
+
 menuButton.addEventListener('click', () => {
     menu.classList.toggle("hidden");
 })
